08ea91551af05d5c783b8e63100519f27649732e
jmt75200 <joelle@pcgamerzhawaii.com>
2015-01-23 11:51:21 -1000

    added scrambled array and tested.

--- generator.js
+++ generator.js
@@ -1,17 +1,37 @@
 module.exports = {
 
-  random : function(n){
+  thousand : function(n){
     var arr = [];
 
     for (var i = 0; i < n; i++ ){
-      arr.push(Math.round(Math.random() * (n)));
+      arr.push(i);
+    }
+
+    //creates an array of 1000 numbers in order
+    return arr;
+
+  },
+
+  scramble : function(array){
+    var swap;
+
+    //in order to scramble the tousand array, the length of an array is divided
+    for (var i = 0; i < array.length / 2; i++){
+      var randomIndex = Math.round(Math.random() * array.length);
+      
+      //swap contains the random pulled numebr from array
+      swap = array[randomIndex];
+      
+      //then store another number into the last spot to swap the two numbers
+      array[randomIndex] = array[i];
+      
+      //then puts swap value into the original index of array[i]
+      array[i] = swap;
 
     }
 
-    return arr;
-
+    return array;
   }
-
 
 
 }; //end of module exports
--- spec/generator-spec.js
+++ spec/generator-spec.js
@@ -2,32 +2,56 @@ var generate = require('../generator.js');
 var n = 1000;
 
 
-describe("generate", function() {
+describe("generate ()", function() {
   it("must contain a function", function() {
     
-    expect( generate.random ).toBeDefined();
+    expect( generate.thousand ).toBeDefined();
 
   });
 
   it("Arguments is a number, greater than 0.", function() {
     
-    var result = generate.random(n);
+    var result = generate.thousand(n);
 
     expect( result.length ).toEqual(n);
     expect( result.length ).toBeGreaterThan(0);
 
   });
 
-  it("Two calls of the random function equal two different values of arrays", function() {
+  it("Two calls of the thousand function equal two different values of arrays", function() {
 
-    var a = generate.random(n);
-    var b = generate.random(n);
+    var a = generate.thousand(n);
+    var b = generate.thousand(n);
 
     if (a === b){
       return false;
     }
 
     return true;
+    
+  });
+
+
+  describe("scramble", function() {
+    it("Must be a function", function() {
+
+      expect( generate.scramble ).toBeDefined();
+
+    });
+
+    it("Result from thousand function must not equal to the result of scramble function", function() {
+
+      var a = generate.thousand(n);
+      var b = generate.scramble(a);
+
+      if (a === b){
+        return false;
+      }
+
+      return true;
+      
+    });
+
     
   });
 
